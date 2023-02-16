d47c68e6e44c6e4e8e0bda533cfd6eb742d01efb
PrDM99 <neodire89@gmail/com>
2023-02-16 10:33:40 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *description : main - prints string to stdout
+ *
+ *Return: 0 if sucess.
+ */
+
+int main(void)
+{
+puts('Programming is like building a multilingual puzzle'\n)
+return (0);
+}
