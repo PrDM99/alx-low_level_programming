6f503c1725bf533e97b8ef2f6fdcd54a46c28c23
PrDM99 <neodire89@gmail/com>
2023-02-21 07:49:40 -0800

    X10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char n;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	_putchar(n);
+	}
+	n++;
+	_putchar('\n');
+	}
+}
+
